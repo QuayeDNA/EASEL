05673bcb8a02f3635c932576588bd8a58713cd71
DNA_Q <34524618+QuayeDNA@users.noreply.github.com>
2023-10-21 19:23:40 +0000

    Testing dev environment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Test from './components/Test'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,8 +27,9 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Viter and React logos to learn more
       </p>
+      <Test />
     </>
   )
 }
